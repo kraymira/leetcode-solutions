bab01918b5ce649626b4ae098328f67f834f539a
Lana Kashirina <fewminutesago@mail.ru>
2025-09-05 23:09:26 +0300

    add math solution

--- easy/9_Palindrome_Number.js
+++ easy/9_Palindrome_Number.js
@@ -9,20 +9,23 @@ var isPalindrome = function(number) {
     return JSON.stringify(s) === JSON.stringify(reversed)
 };
 
-console.log(isPalindrome(121));
-console.log(isPalindrome(10));
-
 /**
  * Without conversion from number
  * 
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome2 = function(number) {
-    if (number < 0) return false;
-
-    let s = number.toString().split('');
-    let reversed = [...s].reverse()
-
-    return JSON.stringify(s) === JSON.stringify(reversed)
+var isPalindrome2 = function(x) {
+    // Handle negative numbers early
+    if (x < 0) return false;
+    
+    let reversed = 0;
+    let original = x;
+    
+    while (x > 0) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    
+    return reversed === original;
 };
